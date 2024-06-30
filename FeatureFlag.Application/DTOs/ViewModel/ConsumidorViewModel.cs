281b9ffebead93ad02c63cd86aed1fa2f2ee51c1
RichardFurlan <emsrichard10@hotmail.com>
2024-06-30 20:16:04 -0300

    Removendo propriedades dos DTOs

--- FeatureFlag.Application/DTOs/ViewModel/ConsumidorViewModel.cs
+++ FeatureFlag.Application/DTOs/ViewModel/ConsumidorViewModel.cs
@@ -2,15 +2,14 @@ namespace FeatureFlag.Application.DTOs.ViewModel;
 
 public record ConsumidorViewModel
 {
-    public ConsumidorViewModel(string identificador, string descricao, List<RecursoViewModel> recursos)
+    public ConsumidorViewModel(string identificacao, string descricao)
     {
-        
-        Identificador = identificador;
+        Identificacao = identificacao;
         Descricao = descricao;
-        Recursos = recursos;
     }
-    public string Identificador { get; init; }
+    
+
+    public string Identificacao { get; init; }
     public string Descricao { get; init; }
-    public List<RecursoViewModel> Recursos { get; init; }
     
 };
