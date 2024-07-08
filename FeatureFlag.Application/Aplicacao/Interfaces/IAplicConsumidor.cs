a193e8363e5455cbd7f5095333259a0b825d34c1
RichardFurlan <emsrichard10@hotmail.com>
2024-07-07 22:11:51 -0300

    Interface novo metodo

--- FeatureFlag.Application/Aplicacao/Interfaces/IAplicConsumidor.cs
+++ FeatureFlag.Application/Aplicacao/Interfaces/IAplicConsumidor.cs
@@ -9,6 +9,7 @@ public interface IAplicConsumidor
     Task<List<ConsumidorViewModel>> ListarTodos();
     Task<ConsumidorViewModel> ListarPorId(int id);
     Task<int> Inserir(CreateConsumidorInputModel createConsumidorInputModel);
+    Task<RecursosPorConsumidorViewModel> RecuperaRecursosPorConsumidor(int id);
     Task Alterar(int id, UpdateConsumidorInputModel updateConsumidorInputModel);
     Task Inativar(int id);
 }
