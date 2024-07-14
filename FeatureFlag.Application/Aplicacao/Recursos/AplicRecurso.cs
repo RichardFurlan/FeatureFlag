ed5ff9b1b4d8a3cbf34471486c85622f7da62b3c
RichardFurlan <emsrichard10@hotmail.com>
2024-07-14 12:38:28 -0300

    Criando metodo AlterarPercentual de liberacao do recurso

--- FeatureFlag.Application/Aplicacao/Recursos/AplicRecurso.cs
+++ FeatureFlag.Application/Aplicacao/Recursos/AplicRecurso.cs
@@ -1,4 +1,5 @@
 using FeatureFlag.Application.Aplicacao.Interfaces;
+using FeatureFlag.Application.Aplicacao.Recursos.DTOs;
 using FeatureFlag.Application.DTOs;
 using FeatureFlag.Application.DTOs.InputModel;
 using FeatureFlag.Application.DTOs.ViewModel;
@@ -12,14 +13,14 @@ namespace FeatureFlag.Application.Aplicacao;
 public class AplicRecurso : IAplicRecurso
 {
     #region ctor
-    private readonly IRepRecurso _repRecursoMemory;
-    private readonly IRepConsumidor _repConsumidoresMemory;
+    private readonly IRepRecurso _repRecurso;
+    private readonly IRepConsumidor _repConsumidores;
     private readonly IRepRecursoConsumidor _repRecursoConsumidor;
 
     public AplicRecurso(IRepRecurso repRecurso, IRepConsumidor repConsumidor, IRepRecursoConsumidor repRecursoConsumidor)
     {
-        _repRecursoMemory = repRecurso;
-        _repConsumidoresMemory = repConsumidor;
+        _repRecurso = repRecurso;
+        _repConsumidores = repConsumidor;
         _repRecursoConsumidor = repRecursoConsumidor;
     }
     #endregion
@@ -27,7 +28,7 @@ public class AplicRecurso : IAplicRecurso
     #region RecuperarTodosAsync
     public async Task<List<RecuperarRecursoDto>> RecuperarTodosAsync(string query)
     {
-        var recursos = await _repRecursoMemory.RecuperarTodosAsync();
+        var recursos = await _repRecurso.RecuperarTodosAsync();
         var viewModelList = recursos.Select(r => new RecuperarRecursoDto(r.Identificacao, r.Descricao)).ToList();
         return viewModelList;
     }
@@ -36,22 +37,22 @@ public class AplicRecurso : IAplicRecurso
     #region RecuperarPorIdAsync
     public async Task<RecuperarRecursoDto> RecuperarPorIdAsync(int id)
     {
-        var recurso = await _repRecursoMemory.RecuperarPorIdAsync(id);
+        var recurso = await _repRecurso.RecuperarPorIdAsync(id);
         var viewModel = new RecuperarRecursoDto(recurso.Identificacao, recurso.Descricao);
         return viewModel;
     }
     #endregion
 
-    #region VerificaRecurso
-    public async Task<RecuperarRecursoAtivoDto> VerificaRecursoAsync(int recursoId, int consumidorId)
+    #region VerificaRecursoAtivoParaConsumidorAsync
+    public async Task<RecuperarRecursoAtivoDto> VerificaRecursoAtivoParaConsumidorAsync(int recursoId, int consumidorId)
     {
-        var recurso = await _repRecursoMemory.RecuperarPorIdAsync(recursoId);
+        var recurso = await _repRecurso.RecuperarPorIdAsync(recursoId);
         if (recurso == null)
         {
             throw new Exception($"Recurso com ID {recursoId} não encontrado.");
         }
         
-        var consumidor = await _repConsumidoresMemory.RecuperarPorIdAsync(consumidorId);
+        var consumidor = await _repConsumidores.RecuperarPorIdAsync(consumidorId);
         if (consumidor == null)
         {
             throw new Exception($"Consumidor com identificação {consumidorId} não encontrado.");
@@ -86,7 +87,7 @@ public class AplicRecurso : IAplicRecurso
             null
         );
 
-        var recursoId = _repRecursoMemory.InserirAsync(recurso);
+        var recursoId = _repRecurso.InserirAsync(recurso);
 
         return recursoId;
     }
@@ -96,9 +97,9 @@ public class AplicRecurso : IAplicRecurso
     public async Task<int> InserirRecursoELiberacaoAsync(CriarRecursoELiberacaoDto criarRecursoELiberacaoDto)
     {
         var recurso = new Recurso(criarRecursoELiberacaoDto.Identificacao, criarRecursoELiberacaoDto.Descricao, null, null);
-        await _repRecursoMemory.InserirAsync(recurso);
+        await _repRecurso.InserirAsync(recurso);
 
-        var todosConsumidores =  await _repConsumidoresMemory.RecuperarTodosAsync();
+        var todosConsumidores =  await _repConsumidores.RecuperarTodosAsync();
         var totalConsumidores = todosConsumidores.Count;
         var quantidadeLiberada = (int)(totalConsumidores * criarRecursoELiberacaoDto.PercentualLiberacao / 100);
 
@@ -116,18 +117,68 @@ public class AplicRecurso : IAplicRecurso
     }
     #endregion
     
+    #region AlterarPercentualDeLiberacaoDeRecurso
+    public async Task AlterarPercentualDeLiberacaoDeRecurso(AlterarPercentualDeLiberacaoRecursoDto alterarPercentualRecursoDto)
+    {
+        var recurso = await _repRecurso.RecuperarPorIdAsync(alterarPercentualRecursoDto.CodigoRecurso);
+        if (recurso == null)
+        {
+            throw new Exception($"Recurso com ID {alterarPercentualRecursoDto.CodigoRecurso} não encontrado.");
+        }
+
+        var todosConsumidores = await _repConsumidores.RecuperarTodosAsync();
+        var totalConsumidores = todosConsumidores.Count;
+        var quantidadeLiberada = (int)(totalConsumidores * alterarPercentualRecursoDto.PercentualLiberacao / 100);
+
+        var random = new Random();
+        var consumidoresLiberados = todosConsumidores.OrderBy(td => random.Next()).Take(quantidadeLiberada).ToList();
+
+        var recursoConsumidoresExistentes = recurso.RecursoConsumidores;
+        foreach (var rce in recursoConsumidoresExistentes)
+        {
+            rce.Desabilitar();
+            await _repRecursoConsumidor.AlterarAsync(rce.Id, rce);
+        }
+        
+        foreach (var consumidor in todosConsumidores)
+        {
+            var status = consumidoresLiberados.Contains(consumidor)
+                ? EnumStatusRecursoConsumidor.Habilitado
+                : EnumStatusRecursoConsumidor.Desabilitado;
+
+            var recursoConsumidor = recurso.RecursoConsumidores.SingleOrDefault(rc => rc.CodigoConsumidor == consumidor.Id);
+
+            if (recursoConsumidor == null)
+            {
+                recursoConsumidor = new RecursoConsumidor(recurso.Id, consumidor.Id, status);
+                await _repRecursoConsumidor.InserirAsync(recursoConsumidor);
+            }
+            else
+            {
+                recursoConsumidor.DefinirStatus(status);
+                await _repRecursoConsumidor.AlterarAsync(recursoConsumidor.Id, recursoConsumidor);
+            }
+        };
+
+    }
+    #endregion
+    
     #region AlterarAsync
     public async Task AlterarAsync(int id, AlterarRecursoDto alterarRecursoDto)
     {
-        var recurso = await _repRecursoMemory.RecuperarPorIdAsync(id);
-        recurso.Update(alterarRecursoDto.Identificacao, alterarRecursoDto.Descricao);
+        var recursoExistente = await _repRecurso.RecuperarPorIdAsync(id);
+        var recursoAlterar = new Recurso(alterarRecursoDto.Identificacao, alterarRecursoDto.Descricao);
+        
+        recursoExistente.Update(recursoAlterar);
+        await _repRecurso.AlterarAsync(id, recursoAlterar);
     }
     #endregion
 
     #region InativarAsync
     public async Task InativarAsync(int id)
     {
-        var recurso = await _repRecursoMemory.RecuperarPorIdAsync(id);
+        var recurso = await _repRecurso.RecuperarPorIdAsync(id);
+        await _repRecurso.InativarAsync(id);
         recurso.Inativar();
     }
 
--- FeatureFlag.Domain/Entities/RecursoConsumidor.cs
+++ FeatureFlag.Domain/Entities/RecursoConsumidor.cs
@@ -4,19 +4,44 @@ namespace FeatureFlag.Domain.Entities;
 
 public class RecursoConsumidor : BaseEntity
 {
-    public RecursoConsumidor(int codigoRecurso, int codigoConsumidor, EnumStatusRecursoConsumidor status)
+    public RecursoConsumidor(int? codigoRecurso, int? codigoConsumidor, EnumStatusRecursoConsumidor? status)
     {
-        CodigoRecurso = codigoRecurso;
-        CodigoConsumidor = codigoConsumidor;
-        Status = status;
+        CodigoRecurso = codigoRecurso ?? CodigoRecurso;
+        CodigoConsumidor = codigoConsumidor ?? CodigoConsumidor;
+        Status = status ?? Status;
     }
 
     public int CodigoRecurso { get; private set; }
     public int CodigoConsumidor { get; private set; }
     public EnumStatusRecursoConsumidor Status { get; private set; }
 
-    public void Update(EnumStatusRecursoConsumidor? status)
+
+    public void Update(int codigoRecurso, int codigoConsumidor, EnumStatusRecursoConsumidor status)
     {
-        Status = status ?? Status;
+        CodigoRecurso = codigoRecurso;
+        CodigoConsumidor = codigoConsumidor;
+        Status = status;
+    }
+
+    public void Desabilitar()
+    {
+        Status = EnumStatusRecursoConsumidor.Desabilitado;
+    }
+    
+    public void Habilitar()
+    {
+        Status = EnumStatusRecursoConsumidor.Habilitado;
+    }
+
+    public void DefinirStatus(EnumStatusRecursoConsumidor status)
+    {
+        if (status == EnumStatusRecursoConsumidor.Habilitado)
+        {
+            Habilitar();
+        }
+        else
+        {
+            Desabilitar();
+        }
     }
 }
