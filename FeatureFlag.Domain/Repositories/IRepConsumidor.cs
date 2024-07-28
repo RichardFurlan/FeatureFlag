3b30c155dd4d56957d6c5c7338b3fe116117d052
Richard Figueiredo Furlan <emsrichard10@hotmail.com>
2024-07-28 15:52:48 -0300

    Interface com novo metodo recuperar

--- FeatureFlag.Domain/Repositories/IRepConsumidor.cs
+++ FeatureFlag.Domain/Repositories/IRepConsumidor.cs
@@ -6,6 +6,7 @@ public interface IRepConsumidor
 {
     Task<List<Consumidor>> RecuperarTodosAsync();
     Task<Consumidor?> RecuperarPorIdAsync(int id);
+    Task<Consumidor?> RecuperarPorIdentificacaoAsync(string identificacaoConsumidor);
     Task<int> InserirAsync(Consumidor consumidor);
     Task AlterarAsync(Consumidor consumidor);
     Task InativarAsync(Consumidor consumidor);
